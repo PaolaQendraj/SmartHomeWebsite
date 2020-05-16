0cccf105a2f4ed430e1f8609b766abefbab7bd7c
PaolaQendraj <paolaqendraj@gmail.com>
2020-05-16 15:39:27 +0200

    changes

--- SmartHome/views.py
+++ SmartHome/views.py
@@ -48,26 +48,68 @@ def contact(request):
         context = {'content1': content1, 'footer1': footer1,'side_menu1': side_menu1}
         return render(request, 'contact.html', context)
     elif request.method == 'POST':
+        from django.core.validators import validate_email
+        from django.core.exceptions import ValidationError
         contactus = request.POST
         name = contactus['name']
         email = contactus['email']
         subject = contactus['subject']
         message = contactus['message']
-        InsertContact=Contact(name=name, email=email, subject=subject, message=message)
-        InsertContact.save()
-        content = Contact_Content.objects.all()
-        content1 = content[0]
-        footer = Footer.objects.all()
-        footer1 = footer[0]
-        side_menu = Side_Menu.objects.all()
-        side_menu1 = side_menu[0]
-        context = {'content1': content1, 'footer1': footer1, 'side_menu1': side_menu1}
-        return render(request, 'contact.html', context)
+        try:
+            validate_email(email)
+            InsertContact = Contact(name=name, email=email, subject=subject, message=message)
+            InsertContact.save()
+            content = Contact_Content.objects.all()
+            content1 = content[0]
+            footer = Footer.objects.all()
+            footer1 = footer[0]
+            side_menu = Side_Menu.objects.all()
+            side_menu1 = side_menu[0]
+            context = {'content1': content1, 'footer1': footer1, 'side_menu1': side_menu1}
+            return render(request, 'contact.html', context)
+        except ValidationError:
+            email_error = "Email i gabuar"
+            content = Contact_Content.objects.all()
+            content1 = content[0]
+            footer = Footer.objects.all()
+            footer1 = footer[0]
+            side_menu = Side_Menu.objects.all()
+            side_menu1 = side_menu[0]
+            context = {'email_error': email_error, 'content1': content1, 'footer1': footer1, 'side_menu1': side_menu1}
+            return render(request, 'contact.html', context)
+
 
 def subscribe(request):
     if request.method == 'POST':
+        from django.core.validators import validate_email
+        from django.core.exceptions import ValidationError
         subscriber = request.POST
         email = subscriber['email']
-        InsertSubscriber=Subscription(email=email)
-        InsertSubscriber.save()
-        return HttpResponseRedirect('/')
+        try:
+            validate_email(email)
+            InsertSubscriber=Subscription(email=email)
+            InsertSubscriber.save()
+            return HttpResponseRedirect('/')
+        except ValidationError:
+            email_error = "Email i gabuar"
+            header = Header.objects.all()
+            header1 = header[0]
+            about = About.objects.all()
+            about1 = about[0]
+            features = Features.objects.all()
+            features1 = features[0]
+            features2 = features[1]
+            features3 = features[2]
+            inovation = Inovation.objects.all()
+            inovation1 = inovation[0]
+            service = Service.objects.all()
+            footer = Footer.objects.all()
+            footer1 = footer[0]
+            side_menu = Side_Menu.objects.all()
+            side_menu1 = side_menu[0]
+            context = {
+                'header1': header1, 'about1': about1, 'features1': features1, 'features2': features2, 'features3':
+                    features3, 'inovation1': inovation1, 'service': service, 'footer1': footer1,
+                'side_menu1': side_menu1, 'email_error': email_error
+            }
+            return render(request, 'index.html', context)
